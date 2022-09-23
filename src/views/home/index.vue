3a4fb5f1ad185b87400175b204e804ef920c2308
Garfield Chen <garfield.chen@qq.com>
2022-09-23 17:22:14 +0800

    sprite test.

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -12,9 +12,6 @@ import * as THREE from 'three';
 
 import Stats from '@/assets/threejs/jsm/libs/stats.module.js';
 
-import {OrbitControls} from '@/assets/threejs/jsm/controls/OrbitControls.js';
-import {GLTFLoader} from '@/assets/threejs/jsm/loaders/GLTFLoader.js';
-
 
 export default {
   components: {
@@ -32,154 +29,109 @@ export default {
     this.threeJs2DScene();
   },
   methods: {
-    threeJsScene() {
+    threeJs2DScene() {
 
-      const container = document.getElementById('three-js');
+      const _threejsContainer = document.getElementById('three-js');
+
+
+      const _winWidth = window.innerWidth;
+      const _winHeight = window.innerHeight;
+
+      let _cameraOrtho, _sceneOrtho;
+      let spriteTL;
+
+      _cameraOrtho = new THREE.OrthographicCamera( - _winWidth / 2, _winWidth / 2, _winHeight / 2, - _winHeight / 2, 1, 10 );
+      _cameraOrtho.position.z = 10;
 
       const stats = new Stats();
-      container.appendChild(stats.dom);
 
-      const scene = new THREE.Scene();
+      _threejsContainer.appendChild(stats.dom);
 
-      scene.background = new THREE.Color('#eee');
+      _sceneOrtho = new THREE.Scene();
 
-      scene.fog = new THREE.Fog('#eee', 20, 100);
+
+      const _textureLoader = new THREE.TextureLoader();
+
+      _textureLoader.load( 'textures/t1.png', createSprites );
 
       const canvas = document.querySelector('#three');
 
       const renderer = new THREE.WebGLRenderer({canvas, antialias: true});
 
-      renderer.shadowMap.enabled = true;
+      renderer.setPixelRatio( window.devicePixelRatio );
+
+      renderer.setSize( window.innerWidth, window.innerHeight );
+
+      renderer.autoClear = false;
+
+      window.addEventListener( 'resize', onWindowResize );
 
 
-      const camera = new THREE.PerspectiveCamera(
-          50,
-          window.innerWidth / window.innerHeight,
-          0.1,
-          1000
-      );
-
-      camera.position.z = 10;
-
-      const gltfLoader = new GLTFLoader();
-
-      const _material = new THREE.RawShaderMaterial( {
-
-        uniforms: {
-          time: { value: 1.0 }
-        },
-        //vertexShader: debris_vertexShader,
-        //fragmentShader: debris_fragmentShader,
-        vertexShader: document.getElementById( 'vertexShader' ).textContent,
-        fragmentShader: document.getElementById( 'fragmentShader' ).textContent,
-        side: THREE.DoubleSide,
-        transparent: true
-
-      } );
-
-      let model;
-      //gltfLoader.load('models/seraphine/scene.gltf', (gltf) => {
-      gltfLoader.load('models/debris_a.glb', (gltf) => {
-
-        model = gltf.scene;
-        //遍历模型每部分
-        model.traverse((o) => {
-        //   //将图片作为纹理加载
-        //   let explosionTexture = new THREE.TextureLoader().load(
-        //       'models/m05.png'
-        //   );
-        //
-        //   //调整纹理图的方向
-        //   explosionTexture.flipY = false;
-        //
-        //   //将纹理图生成基础网格材质(MeshBasicMaterial)
-        //   const material = new THREE.MeshBasicMaterial({
-        //     map: explosionTexture,
-        //   });
-        //
-        //   //给模型每部分上材质
-           o.material = _material;
-        //
-        //   if (o.isMesh) {
-        //     o.castShadow = true;
-        //     o.receiveShadow = true;
-        //   }
-        })
-        scene.add(model);
-      }, undefined, function (e) {
-
-        console.log(e);
-
-      })
-
-      const hemLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.6);
-      hemLight.position.set(0, 48, 0);
-      scene.add(hemLight);
-
-      const dirLight = new THREE.DirectionalLight(0xffffff, 0.6);
-
-      //光源等位置
-      dirLight.position.set(-10, 8, -5);
-
-      //可以产生阴影
-      dirLight.castShadow = false;
-      dirLight.shadow.mapSize = new THREE.Vector2(1024, 1024);
-      scene.add(dirLight);
-
-      let floorGeometry = new THREE.PlaneGeometry(8000, 8000);
-      let floorMaterial = new THREE.MeshPhongMaterial({
-        color: 0x857ebb,
-        shininess: 0,
-      });
-
-      let floor = new THREE.Mesh(floorGeometry, floorMaterial);
-      floor.rotation.x = -0.5 * Math.PI;
-      floor.receiveShadow = true;
-      floor.position.y = -10.001;
-      scene.add(floor);
-
-      const controls = new OrbitControls(camera, renderer.domElement);
-      controls.enableDamping = true;
-
-      function animate() {
-        stats.update();
-        controls.update();
-        renderer.render(scene, camera);
-        requestAnimationFrame(animate);
-
-        if (resizeRendererToDisplaySize(renderer)) {
-          const canvas = renderer.domElement;
-          camera.aspect = canvas.clientWidth / canvas.clientHeight;
-          camera.updateProjectionMatrix();
-        }
-
-        const time = performance.now();
-        //const object = scene.children[ 0 ];
-
-        //debugger
-        if ( model )
-        {
-          model.rotation.y = time * 0.0005;
-          model.material.uniforms.time.value = time * 0.005;
-        }
-
-      }
 
       animate();
 
-      function resizeRendererToDisplaySize(renderer) {
-        const canvas = renderer.domElement;
-        var width = window.innerWidth;
-        var height = window.innerHeight;
-        var canvasPixelWidth = canvas.width / window.devicePixelRatio;
-        var canvasPixelHeight = canvas.height / window.devicePixelRatio;
+      function createSprites( _texture )
+      {
 
-        const needResize = canvasPixelWidth !== width || canvasPixelHeight !== height;
-        if (needResize) {
-          renderer.setSize(width, height, false);
-        }
-        return needResize;
+        const _material = new THREE.SpriteMaterial( { map: _texture} );
+
+        _material.map.offset = new THREE.Vector2(0.25, 0.25);
+        _material.map.repeat = new THREE.Vector2(0.25, 0.25);
+
+        const width = _material.map.image.width / 4;
+        const height = _material.map.image.height / 4;
+
+        spriteTL = new THREE.Sprite( _material );
+
+        spriteTL.center.set( 0.5, 0.5 );
+
+        spriteTL.scale.set( width, height, 1 );
+
+        _sceneOrtho.add( spriteTL );
+
+        updateSprites();
       }
+
+      function onWindowResize() {
+
+        const width = window.innerWidth;
+        const height = window.innerHeight;
+
+        _cameraOrtho.left = - width / 2;
+        _cameraOrtho.right = width / 2;
+        _cameraOrtho.top = height / 2;
+        _cameraOrtho.bottom = - height / 2;
+        _cameraOrtho.updateProjectionMatrix();
+
+        updateSprites();
+
+        renderer.setSize( window.innerWidth, window.innerHeight );
+
+      }
+
+      function animate() {
+        stats.update();
+        requestAnimationFrame(animate);
+        render();
+      }
+
+      function render(){
+
+        renderer.clear();
+        renderer.clearDepth();
+        renderer.render( _sceneOrtho, _cameraOrtho );
+
+      }
+
+      function updateSprites() {
+
+        const width = window.innerWidth / 2;
+        const height = window.innerHeight / 2;
+
+        spriteTL.position.set( - width, height, 1 ); // top left
+
+      }
+
     },
   },
 };
